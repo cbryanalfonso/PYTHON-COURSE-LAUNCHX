b23767b7e2043771b5cd6942a49967946fdd4dcd
cbryanalfonso <cbryanalfonso@gmail.com>
2022-02-06 21:08:11 -0600

    Curso python

---
+++ MODULO 1 - PRIMEROS PASOS/modulo1.py
@@ -0,0 +1,18 @@
+sum = 1+2
+print(sum)
+
+print('HOLA DESDE LA CONSOLA ...')
+
+#VARIABLES...
+
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+#COMO RESULTADO SE ENTREGAO CON 6.
+
+# Declaramos la variable
+distancia_a_alfa_centauri = 4.367
+
+# Descubrimos su tipo de dato
+type(distancia_a_alfa_centauri)
+
