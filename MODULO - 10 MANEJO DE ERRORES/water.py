550774709016713f7da976d24684ff8c3fafa447
cbryanalfonso <cbryanalfonso@gmail.com>
2022-02-13 20:09:12 -0600

    modify:Modules7-10

---
+++ MODULO - 10 MANEJO DE ERRORES/water.py
@@ -0,0 +1,33 @@
+# Manejo de excepciones
+
+# def water_left(astronauts, water_left, days_left):
+#     daily_usage = astronauts * 11
+#     total_usage = daily_usage * days_left
+#     total_water_left = water_left - total_usage
+#     return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+# def water_left(astronauts, water_left, days_left):
+#     daily_usage = astronauts * 11
+#     total_usage = daily_usage * days_left
+#     total_water_left = water_left - total_usage
+#     if total_water_left < 0:
+#         raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+#     return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            argument / 10
+        except TypeError:
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+try:
+    water_left(3, 200, None)
+except RuntimeError as err:
+    alert_navigation_system(err)
